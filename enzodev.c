e52016920eb67ee3a7b83350e9cc193bf445995f
enferreirazo <Enzo.ferreira2507@gmail.com>
2025-08-12 22:16:20 -0300

    enzodev n 1 .c

---
+++ enzodev.c
@@ -0,0 +1,17 @@
+/*
+ * Lab. 1
+ * Exemplo
+ */
+ 
+ #include <stdio.h>
+ 
+ int main() {
+ 	// imprime algumas mensanges na tela 
+ 	printf("mackenzie - FCI\n");
+ 	printf("algoritimo e ");
+ 	printf("prog. II\n");
+ 	
+ 	//Pausa antes de terminar  o programa
+ 	//system("pause");
+ 	return 0;
+ }
